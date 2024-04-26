3089b08a5cb9d412913a4e26f281bc90e10be600
Jguerra47 <juan.guerra@mercadolibre.com.co>
2024-04-26 12:29:05 -0500

    Initial scaffolding

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -3,7 +3,6 @@ import {getRepository} from "typeorm";
 import {User} from "../entity/user.entity";
 import bcryptjs from 'bcryptjs';
 import {sign, verify} from "jsonwebtoken";
-import {Order} from "../entity/order.entity";
 
 export const Register = async (req: Request, res: Response) => {
     const {password, password_confirm, ...body} = req.body;
@@ -25,46 +24,6 @@ export const Register = async (req: Request, res: Response) => {
     res.send(user);
 }
 
-export const Login = async (req: Request, res: Response) => {
-    const user = await getRepository(User).findOne({email: req.body.email}, {
-        select: ["id", "password", "is_ambassador"]
-    });
-
-    if (!user) {
-        return res.status(400).send({
-            message: 'invalid credentials!'
-        });
-    }
-
-    if (!await bcryptjs.compare(req.body.password, user.password)) {
-        return res.status(400).send({
-            message: 'invalid credentials!'
-        });
-    }
-
-    const adminLogin = req.path === '/api/admin/login';
-
-    if (user.is_ambassador && adminLogin) {
-        return res.status(401).send({
-            message: 'unauthorized'
-        });
-    }
-
-    const token = sign({
-        id: user.id,
-        scope: adminLogin ? "admin" : "ambassador"
-    }, process.env.SECRET_KEY);
-
-    res.cookie("jwt", token, {
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000//1 day
-    })
-
-    res.send({
-        message: 'success'
-    });
-}
-
 export const AuthenticatedUser = async (req: Request, res: Response) => {
     const user = req["user"];
 
@@ -72,26 +31,21 @@ export const AuthenticatedUser = async (req: Request, res: Response) => {
         return res.send(user);
     }
 
-    const orders = await getRepository(Order).find({
-        where: {
-            user_id: user.id,
-            complete: true
-        },
-        relations: ['order_items']
-    });
+    // const orders = await getRepository(Order).find({
+    //     where: {
+    //         user_id: user.id,
+    //         complete: true
+    //     },
+    //     relations: ['order_items']
+    // });
 
-    user.revenue = orders.reduce((s, o) => s + o.ambassador_revenue, 0);
+    // TODO: axios get orders
+
+    // user.revenue = orders.reduce((s, o) => s + o.ambassador_revenue, 0);
 
     res.send(user);
 }
 
-export const Logout = async (req: Request, res: Response) => {
-    res.cookie("jwt", "", {maxAge: 0});
-
-    res.send({
-        message: 'success'
-    });
-}
 
 export const UpdateInfo = async (req: Request, res: Response) => {
     const user = req["user"];
@@ -101,20 +55,4 @@ export const UpdateInfo = async (req: Request, res: Response) => {
     await repository.update(user.id, req.body);
 
     res.send(await repository.findOne(user.id));
-}
-
-export const UpdatePassword = async (req: Request, res: Response) => {
-    const user = req["user"];
-
-    if (req.body.password !== req.body.password_confirm) {
-        return res.status(400).send({
-            message: "Password's do not match!"
-        })
-    }
-
-    await getRepository(User).update(user.id, {
-        password: await bcryptjs.hash(req.body.password, 10)
-    });
-
-    res.send(user);
 }
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -16,14 +16,6 @@ export class User {
     })
     email: string;
 
-    @Column({
-        select: false
-    })
-    password: string;
-
-    @Column()
-    is_ambassador: boolean;
-
     get name(): string {
         return this.first_name + ' ' + this.last_name;
     }
