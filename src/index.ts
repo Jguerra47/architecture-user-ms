7221eeff091db43fa3b2c0f927c2d6d0b7461d62
drew <58788781+drew138@users.noreply.github.com>
2024-04-26 23:54:19 -0500

    feat: wip

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ createConnection().then(async () => {
     app.use(express.json());
     app.use(cors({
         credentials: true,
-        origin: ['http://localhost:3000', 'http://localhost:4000', 'http://localhost:5000']
+        origin: ['*']
     }));
 
     routes(app);
