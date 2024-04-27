a5f6b68029af39d7a3803689b72f6875e4afacde
Samuel Villegas <villegassamuel25@gmail.com>
2024-04-27 00:04:43 -0500

    Change ambassador validation

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,114 +1,127 @@
-import {Request, Response} from "express";
-import {getConnection, getRepository} from "typeorm";
-import {User} from "../entity/user.entity";
+import { Request, Response } from "express";
+import { getConnection, getRepository } from "typeorm";
+import { User } from "../entity/user.entity";
 import axios from "axios";
 
 export const Register = async (req: Request, res: Response) => {
-    const { password, password_confirm, email } = req.body;
+  const { password, password_confirm, email } = req.body;
 
-    if (password !== password_confirm) {
-        return res.status(400).send({
-            message: "Password's do not match!"
-        })
-    }
+  if (password !== password_confirm) {
+    return res.status(400).send({
+      message: "Password's do not match!",
+    });
+  }
 
-    const emailUsed = await getRepository(User).find({
-            email
-    })
-    if (emailUsed.length > 0) {
-        return res.status(400).send({
-            message: "Email already registered"
-        })
-    }
+  const emailUsed = await getRepository(User).find({
+    email,
+  });
+  if (emailUsed.length > 0) {
+    return res.status(400).send({
+      message: "Email already registered",
+    });
+  }
 
-    const user = await startRegisterTransaction(req)
-    if(!user){
-        return res.send({
-            message: "Internal server error"
-        }).status(500);
-    }
-    res.send(user);
-}
+  const user = await startRegisterTransaction(req);
+  if (!user) {
+    return res
+      .send({
+        message: "Internal server error",
+      })
+      .status(500);
+  }
+  res.send(user);
+};
 
 async function startRegisterTransaction(req: Request) {
-    const {password, first_name, last_name, email} = req.body;
-    const queryRunner = getConnection().createQueryRunner();
+  const { password, first_name, last_name, email } = req.body;
+  const queryRunner = getConnection().createQueryRunner();
 
-    try {
-        await queryRunner.connect();
-        await queryRunner.startTransaction();
+  try {
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-        const user = new User()
-        user.first_name = first_name
-        user.last_name = last_name
-        user.email = email
-        user.is_ambassador = req.path === '/api/user/ambassador/register'
+    const user = new User();
+    user.first_name = first_name;
+    user.last_name = last_name;
+    user.email = email;
+    user.is_ambassador = req.path === "/api/user/ambassador/register";
 
-        const userSaved = await queryRunner.manager.save(user);
+    const userSaved = await queryRunner.manager.save(user);
 
-        await sendToAuthMs(userSaved.id, password, email, userSaved.is_ambassador)
+    await sendToAuthMs(
+      userSaved.id,
+      password,
+      email,
+      req.path === "/api/user/ambassador/register"
+    );
 
-        await queryRunner.commitTransaction();
-        return userSaved;
-    } catch (e) {
-        await queryRunner.rollbackTransaction()
-        console.error(e);
-        return false;   
-    }
+    await queryRunner.commitTransaction();
+    return userSaved;
+  } catch (e) {
+    await queryRunner.rollbackTransaction();
+    console.error(e);
+    return false;
+  }
 }
 
-async function sendToAuthMs(user_id: number, password: string, email: string, is_ambassador: boolean) {
-    const userDetailsDTO = {
-        user_id,
-        password: password,
-        email,
-        is_ambassador
-    }
+async function sendToAuthMs(
+  user_id: number,
+  password: string,
+  email: string,
+  is_ambassador: boolean
+) {
+  const userDetailsDTO = {
+    user_id,
+    password: password,
+    email,
+    is_ambassador,
+  };
 
-    const url = process.env.AUTH_BASE_URL;
+  const url = process.env.AUTH_BASE_URL;
 
-    await axios.post(`http://${url}/api/auth/register`, userDetailsDTO);
+  await axios.post(`http://${url}/api/auth/register`, userDetailsDTO);
 }
 
 export const AuthenticatedUser = async (req: Request, res: Response) => {
-    const user = req["user"];
+  const user = req["user"];
 
-    if (req.path === '/api/admin/user') {
-        return res.send(user);
-    }
+  if (req.path === "/api/admin/user") {
+    return res.send(user);
+  }
 
-    // const orders = await getRepository(Order).find({
-    //     where: {
-    //         user_id: user.id,
-    //         complete: true
-    //     },
-    //     relations: ['order_items']
-    // });
+  // const orders = await getRepository(Order).find({
+  //     where: {
+  //         user_id: user.id,
+  //         complete: true
+  //     },
+  //     relations: ['order_items']
+  // });
 
-    // TODO: axios get orders
-    //
-    interface order {
-        ambassador_revenue: number
-    }
+  // TODO: axios get orders
+  //
+  interface order {
+    ambassador_revenue: number;
+  }
 
-    const url = process.env.NODE_AMBASSADOR_BASE_URL;
+  const url = process.env.NODE_AMBASSADOR_BASE_URL;
 
-    const orderres = await axios.get<order[]>(`http://${url}/api/admin/orders`, { params: { user_id: user.id } })
+  const orderres = await axios.get<order[]>(`http://${url}/api/admin/orders`, {
+    params: { user_id: user.id },
+  });
 
-    const orders = orderres.data;
+  const orders = orderres.data;
 
-    user.revenue = orders.reduce((s, o) => s + o.ambassador_revenue, 0);
+  user.revenue = orders.reduce((s, o) => s + o.ambassador_revenue, 0);
 
-    res.send(user);
-}
-
+  res.send(user);
+};
 
 export const UpdateInfo = async (req: Request, res: Response) => {
-    const user = req["user"];
+  const user = req["user"];
 
-    const repository = getRepository(User);
+  const repository = getRepository(User);
 
-    await repository.update(user.id, req.body);
+  await repository.update(user.id, req.body);
 
-    res.send(await repository.findOne(user.id));
-}
+  res.send(await repository.findOne(user.id));
+};
